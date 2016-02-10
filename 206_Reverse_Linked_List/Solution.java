c888fdf7f83f92a2ea1459d33d44d54621000194
christinazou <xinguanzxk@126.com>
2016-02-10 15:43:42 -0500

    Update Solution.java

--- 206_Reverse_Linked_List/Solution.java
+++ 206_Reverse_Linked_List/Solution.java
@@ -1,3 +1,7 @@
+//Reverse a singly linked list.
+//Hint:
+//A linked list can be reversed either iteratively or recursively. Could you implement both?
+
 /**
  * Definition for singly-linked list.
  * public class ListNode {
@@ -6,6 +10,9 @@
  *     ListNode(int x) { val = x; }
  * }
  */
+ 
+ //My solution
+ // iterative solution
 public class Solution {
     public ListNode reverseList(ListNode head) {
             ListNode prev = null;
@@ -18,3 +25,62 @@ public class Solution {
     return prev;
     }
 }
+
+// recursive solution
+public ListNode reverseList(ListNode head) {
+    return reverseListInt(head, null);
+}
+
+public ListNode reverseListInt(ListNode head, ListNode newHead) {
+    if(head == null)
+        return newHead;
+    ListNode next = head.next;
+    head.next = newHead;
+    return reverseListInt(next, head);
+}
+
+
+//another recursive solution
+public class Solution {
+    public ListNode reverseList(ListNode head) {
+        if(head==null || head.next==null)
+            return head;
+        ListNode nextNode=head.next;
+        ListNode newHead=reverseList(nextNode);
+        nextNode.next=head;
+        head.next=null;
+        return newHead;
+    }
+}
+
+//another recursive solution
+//and its cleanup version
+public class Solution {
+    public ListNode reverseList(ListNode curr){
+        if(curr==null) return curr;
+        ListNode rest = curr.next;
+        if(rest==null) return curr;
+        rest = reverseList(rest);
+        curr.next.next = curr;    //  Here is the trick
+        curr.next =null;          //
+        return rest;
+    }
+}
+
+   public class Solution {
+        public ListNode reverseList(ListNode head) {
+            if (head == null || head.next == null) {
+                return head;
+            } else {
+                ListNode next = head.next;
+                next = reverseList(next);
+
+                head.next.next = head;
+                head.next = null;
+
+                return next;   
+            }
+        }
+    }
+
+
