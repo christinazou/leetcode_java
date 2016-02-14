91ab819f301308070a04c374408e5c98e5dadfef
christinazou <xinguanzxk@126.com>
2016-02-13 22:24:16 -0500

    Update Solution.java

--- 24_Swap_Nodes_in_Pairs/Solution.java
+++ 24_Swap_Nodes_in_Pairs/Solution.java
@@ -14,3 +14,32 @@ public ListNode swapPairs(ListNode head) {
     }
     return dummy.next;
 }
+
+// This is similar to my intuitive thought. However it doesn't use an dummy node.
+public class Solution {
+        public ListNode swapPairs(ListNode head) {
+            ListNode cur = head, next1, next2;
+            if(head == null) return null;
+            if(head.next == null) return head;
+
+            //head change with next is a special case, so we deal with it first
+            ListNode temp = cur.next;
+            cur.next = temp.next;
+            temp.next = cur;
+            head = temp;
+
+            while(cur.next != null && cur.next.next != null) {
+                next1 = cur.next;
+                next2 = cur.next.next;
+                swap(cur,next1,next2);
+                cur = cur.next.next;
+            }
+            return head;
+        }
+
+        private void swap(ListNode cur, ListNode next1, ListNode next2) {
+            cur.next = next2;
+            next1.next = next2.next;
+            next2.next = next1;
+        }
+    }
