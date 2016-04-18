89f86df6a9652493ba495de6715cb0e46c3ba947
christinazou <xinguanzxk@126.com>
2016-04-18 19:43:00 -0400

    Update MainArrays.java

--- MainArrays.java
+++ MainArrays.java
@@ -1,16 +1,16 @@
+
+import java.util.Arrays;
+
 public class MainArrays
 {
   public static void main(String[] args)
   {
     int[] t1= {-1};
-     int[] t2= {1,2,3,4,5};
-     A a= new A();
-         int[] b1=a.rotate(t2,2);
-         System.out.println("Real prime numbers : " + Arrays.toString(b1));
-int[] a1=a.rotate(t1,2);
-
+    int[] t2= {1,2,3,4,5};
+    A a= new A();
+    int[] b1=a.rotate(t2,2);
+    System.out.println("Real prime numbers : " + Arrays.toString(b1));
+    int[] a1=a.rotate(t1,2);
     System.out.println("Real prime numbers : " + Arrays.toString(a1));
-    
-    
   }
 }
