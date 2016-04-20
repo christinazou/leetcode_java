cfd0b2a7ae1403471ea6225a5fc46f9db440f77e
christinazou <xinguanzxk@126.com>
2016-04-19 20:40:38 -0400

    Create 283_Move_Zeroes.java

---
+++ 283_Move_Zeroes.java
@@ -0,0 +1,19 @@
+public class Solution {
+    public void moveZeroes(int[] nums) {
+        //reverse bubble sort method
+        int sorted=0;
+        int temp=0;
+        //find !0
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+               //insert !0 in sorted
+               for(int j=i;j>sorted;j--){
+                   temp=nums[j];
+                   nums[j]=nums[j-1];
+                   nums[j-1]=temp;
+               }
+               sorted=sorted+1;
+            }
+        }
+    }
+}
