f21681804cc88e63dae35ed507ddf6a5451eddcf
christinazou <xinguanzxk@126.com>
2016-04-19 20:50:03 -0400

    Update 283_Move_Zeroes.java

--- 283_Move_Zeroes.java
+++ 283_Move_Zeroes.java
@@ -16,4 +16,24 @@ public class Solution {
             }
         }
     }
+    
+    
+    public void moveZeroes(int[] nums) {
+        // insert sort method
+        int sorted=0;
+        int temp=0;
+        //find !0
+        for(int i=0;i<nums.length;i++){
+            if(nums[i]!=0){
+               //insert !0 in sorted
+               nums[sorted]=nums[i];
+               sorted=sorted+1;
+                nums[i]=0;
+            }
+        }
+        for(int i=sorted;i<nums.length;i++){
+            nums[i]=0;
+        }
+    }
+    
 }
