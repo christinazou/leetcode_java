fddd981b4b9436c78d554394346f6d4ff7357b00
christinazou <xinguanzxk@126.com>
2016-04-21 13:55:32 -0400

    Update Arrays_119_Pascal's_Triangle_II.java

--- Arrays_119_Pascal's_Triangle_II.java
+++ Arrays_119_Pascal's_Triangle_II.java
@@ -14,5 +14,24 @@ public class Solution {
         }
         return rs;
     }
-    
+}
+
+// use two arrays to use only O(k) extra space?
+public class Solution {
+    public List<Integer> getRow(int rowIndex) {
+       LinkedList rs=new LinkedList();
+       int[] a=new int[rowIndex+2];
+       int[] temp=new int[rowIndex+2];
+        a[1]=1;
+        for(int i=1;i<=rowIndex+1;i++){
+            System.arraycopy(a, 0, temp, 0, rowIndex+1);
+            for(int j=1;j<=i;j++){
+                if(i>=2){
+                    a[j]=temp[j-1]+temp[j];
+                }
+                if(i==rowIndex+1){rs.add(a[j]);}
+            }
+        }
+        return rs;
+    }
 }
