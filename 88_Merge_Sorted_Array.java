f9d6c138516c2aa0ce2a39e26d9ec81b432f756b
christinazou <xinguanzxk@126.com>
2016-04-22 02:57:09 -0400

    Create 88_Merge_Sorted_Array.java

---
+++ 88_Merge_Sorted_Array.java
@@ -0,0 +1,31 @@
+public class Solution {
+    public void merge(int[] nums1, int m, int[] nums2, int n) {
+        int[] rs= new int[m+n];
+        int c1=0,c2=0;
+        for(int i=0;i<m+n;i++){
+        //    System.out.println("i"+i+" c1"+c1+" c2"+c2);
+            if(c1<m&&c2<n){
+                if(nums1[c1]<nums2[c2]){
+                   rs[i]=nums1[c1];
+                   c1++; 
+                }
+                else{
+                rs[i]=nums2[c2];
+                c2++;
+                }
+            }
+            else if(c1<m){
+                rs[i]=nums1[c1];
+                c1++; 
+            }
+            else if(c2<n){
+                rs[i]=nums2[c2];
+                c2++;
+            }
+        }
+       //  nums1[0]=10;
+          //System.out.println(n+m);
+         // rs[0]=10;
+        System.arraycopy(rs, 0, nums1, 0, n+m);
+    }
+}
