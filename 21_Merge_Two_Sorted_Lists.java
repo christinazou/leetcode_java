b5beaca181f0f48e11cc15464cee38357c6b23bc
christinazou <xinguanzxk@126.com>
2016-04-29 20:32:35 -0400

    Update 21_Merge_Two_Sorted_Lists.java

--- 21_Merge_Two_Sorted_Lists.java
+++ 21_Merge_Two_Sorted_Lists.java
@@ -7,24 +7,21 @@
  * }
  */
 public class Solution {
-    public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
-        
-ListNode dummy= new ListNode(0);
-dummy.next=l1;
-ListNode p=l1;
-l1=dummy;
-
-while(p!=null&&l2!=null)
-{if(l2.val>p.val)
-{l1=p;
-p=p.next;
-}
-else{
-l1.next=l2;
-l1=l2;
-l2=l2.next;
-l1.next=p;
-}
+public ListNode mergeTwoLists(ListNode l1, ListNode l2) {
+    ListNode dummy= new ListNode(0);
+    dummy.next=l1;
+    ListNode p=l1;
+    l1=dummy;
+    while(p!=null&&l2!=null)
+    {if(l2.val>p.val){
+        l1=p;
+        p=p.next;
+    }else{
+        l1.next=l2;
+        l1=l2;
+        l2=l2.next;
+        l1.next=p;
+    }
 }
 if(l2!=null){
     l1.next=l2;}
